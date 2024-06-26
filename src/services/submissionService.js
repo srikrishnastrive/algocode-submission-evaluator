225b27357309c7423461921abfd10c0bd4e94b09
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-06-26 16:20:21 +0530

    added the api services

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,3 +1,4 @@
+const { fetchProblemDetails } = require("../apis/problemAdminApi");
 const submissionQueueProducer = require("../producers/submissionQueueProducer");
 
 
@@ -6,15 +7,57 @@ class SubmissionService{
     constructor(submissionRepository){
         this.submissionRepository = submissionRepository;
     }
-    async addSubmission(submissionPayload){
+    // async addSubmission(submissionPayload){
+    //     //Hit the problem admin service and fetch the problem details
+    //     //we are getting the problem Id from the front end
+    //     const problemId = submissionPayload.problemId;
+
+    //     const problemAdminResponse = await fetchProblemDetails(problemId);
+    //     console.log(problemAdminResponse);
+    //     return true;
+
+    //     //we are going to create entry in db.
+    //     // const submission = await this.submissionRepository.createSubmission(submissionPayload);
+    //     // if(!submission){
+    //     //     throw {message:"Not able to crate submission"}
+    //     // }
+    //     // console.log(submission);
+    //     // const response = await submissionQueueProducer(submission);
+    //     // return {queueResponse: response, submission};
+        
+    // }
+    async addSubmission(submissionPayload) {
+        // Hit the problem admin service and fetch the problem details
+        const problemId = submissionPayload.problemId;
+
+        const problemAdminApiResponse = await fetchProblemDetails(problemId);
+
+        if(!problemAdminApiResponse) {
+            throw new SubmissionCreationError('Failed to create a submission in the repository');
+        }
+
+        const languageCodeStub = problemAdminApiResponse.data.codeStubs.find(codeStub => codeStub.language.toLowerCase() === submissionPayload.language.toLowerCase());
+
+        console.log(languageCodeStub) 
+
+        submissionPayload.code = languageCodeStub.startSnippet + "\n\n" + submissionPayload.code + "\n\n" + languageCodeStub.endSnippet;
+
+
         const submission = await this.submissionRepository.createSubmission(submissionPayload);
-        if(!submission){
-            throw {message:"Not able to crate submission"}
+        if(!submission) {
+            // TODO: Add error handling here
+            throw new SubmissionCreationError('Failed to create a submission in the repository');
         }
         console.log(submission);
-        const response = await submissionQueueProducer(submission);
+        const response = await submissionQueueProducer({
+            [submission._id]: {
+                code: submission.code,
+                language: submission.language,
+                inputCase: problemAdminApiResponse.data.testCases[0].input,
+                outputCase: problemAdminApiResponse.data.testCases[0].output,
+            }
+        });
         return {queueResponse: response, submission};
-        
     }
 }
 
